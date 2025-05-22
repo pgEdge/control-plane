7e9a25244c594abff73ff2ea4577884681eb0e92
Jason Lynch <jason@pgedge.com>
2025-05-22 15:02:11 -0400

    ci: add goreleaser
    Adds goreleaser for release automation.
    
    PLAT-55

--- tools.mk
+++ tools.mk
@@ -4,6 +4,7 @@ gobin=$(or $(shell go env GOBIN),$(shell go env GOPATH)/bin)
 gotestsum=$(gobin)/gotestsum
 golangcilint=$(gobin)/golangci-lint
 goa=$(gobin)/goa
+goreleaser=$(gobin)/goreleaser
 
 .PHONY: install-tools
 install-tools:
@@ -11,3 +12,5 @@ install-tools:
 	go install github.com/golangci/golangci-lint/v2/cmd/golangci-lint@v2.1.5
 	go install google.golang.org/protobuf/cmd/protoc-gen-go@v1.35.2
 	go install goa.design/goa/v3/cmd/goa@v3.19.1
+	go install github.com/goreleaser/goreleaser/v2@latest
+	go install github.com/anchore/syft/cmd/syft@v1.25.1
