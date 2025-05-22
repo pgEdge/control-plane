2704f1ad998166e320ce5c4d6ee5988b44c3215a
Jason Lynch <jason@pgedge.com>
2025-05-22 15:03:50 -0400

    ci: add changie
    Adds changie for changelog automation.
    
    PLAT-55

--- tools.mk
+++ tools.mk
@@ -1,10 +1,10 @@
-# TODO: go 1.24 has official support for tools in go.mod:  https://tip.golang.org/doc/go1.24#tools
 gobin=$(or $(shell go env GOBIN),$(shell go env GOPATH)/bin)
 
 gotestsum=$(gobin)/gotestsum
 golangcilint=$(gobin)/golangci-lint
 goa=$(gobin)/goa
 goreleaser=$(gobin)/goreleaser
+changie=$(gobin)/changie
 
 .PHONY: install-tools
 install-tools:
@@ -14,3 +14,4 @@ install-tools:
 	go install goa.design/goa/v3/cmd/goa@v3.19.1
 	go install github.com/goreleaser/goreleaser/v2@latest
 	go install github.com/anchore/syft/cmd/syft@v1.25.1
+	go install github.com/miniscruff/changie@v1.21.1
