35ab8cba256a2988ed60a75ae26ff0f8aca60faf
Jason Lynch <jason@pgedge.com>
2025-10-27 11:10:59 -0400

    fix: access logs (#157)
    These middleware handlers were applied in the wrong order, so the logger
    was nil in our log method. Reversing the order fixes the issue. I've
    also changed the levels so that `/v1/version` requests are logged at the
    debug level and all other requests are logged at the info level. This
    prevents log spam from health checks.

--- server/internal/api/middleware.go
+++ server/internal/api/middleware.go
@@ -10,19 +10,18 @@ import (
 
 func addMiddleware(logger zerolog.Logger, next http.Handler) http.Handler {
 	for _, m := range []func(http.Handler) http.Handler{
-		hlog.NewHandler(logger),
-		hlog.URLHandler("http.url"),
-		hlog.MethodHandler("http.method"),
-		hlog.RemoteIPHandler("http.client_ip"),
-		hlog.UserAgentHandler("http.useragent"),
 		hlog.AccessHandler(func(r *http.Request, status, size int, duration time.Duration) {
 			log := zerolog.Ctx(r.Context())
 
 			var evt *zerolog.Event
-			if status >= 400 && status <= 599 {
+			switch {
+			case status >= 400 && status <= 599:
 				evt = log.Error()
-			} else {
+			case r.URL.Path == "/v1/version":
+				// The version endpoint is used for health checks
 				evt = log.Debug()
+			default:
+				evt = log.Info()
 			}
 
 			// DataDog wants nanoseconds in the duration field. The Separate ms
@@ -33,6 +32,11 @@ func addMiddleware(logger zerolog.Logger, next http.Handler) http.Handler {
 				Int("http.response.content_length", size).
 				Msg("http request")
 		}),
+		hlog.URLHandler("http.url"),
+		hlog.MethodHandler("http.method"),
+		hlog.RemoteIPHandler("http.client_ip"),
+		hlog.UserAgentHandler("http.useragent"),
+		hlog.NewHandler(logger),
 	} {
 		next = m(next)
 	}
