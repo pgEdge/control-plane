d1b4d5430f191169989c930018a1291b8751c616
Jason Lynch <jason@pgedge.com>
2025-11-02 16:38:20 -0500

    chore: remove unused code

--- server/internal/orchestrator/swarm/utils.go
+++ server/internal/orchestrator/swarm/utils.go
@@ -6,12 +6,10 @@ import (
 	"fmt"
 	"io"
 	"strings"
-	"time"
 
 	"github.com/docker/docker/api/types"
 	"github.com/docker/docker/api/types/container"
 	"github.com/docker/docker/api/types/filters"
-	"github.com/docker/docker/api/types/swarm"
 
 	"github.com/pgEdge/control-plane/server/internal/docker"
 	"github.com/pgEdge/control-plane/server/internal/ds"
@@ -48,39 +46,6 @@ func PostgresContainerExec(ctx context.Context, w io.Writer, dockerClient *docke
 	err = dockerClient.Exec(ctx, w, container.ID, cmd)
 	if err != nil {
 		return fmt.Errorf("failed to exec command in postgres container: %w", err)
-	}
-	return nil
-}
-
-func GetPostgresService(ctx context.Context, dockerClient *docker.Docker, instanceID string) (swarm.Service, error) {
-	matches, err := dockerClient.ServiceList(ctx, types.ServiceListOptions{
-		Filters: filters.NewArgs(
-			filters.Arg("label", fmt.Sprintf("pgedge.instance.id=%s", instanceID)),
-			filters.Arg("label", fmt.Sprintf("pgedge.component=%s", "postgres")),
-		),
-	})
-	if err != nil {
-		return swarm.Service{}, fmt.Errorf("failed to list services: %w", err)
-	}
-	if len(matches) == 0 {
-		return swarm.Service{}, fmt.Errorf("%w: %q", ErrNoPostgresService, instanceID)
-	}
-	return matches[0], nil
-}
-
-func PostgresServiceScale(ctx context.Context, dockerClient *docker.Docker, instanceID string, replicas int, timeout time.Duration) error {
-	service, err := GetPostgresService(ctx, dockerClient, instanceID)
-	if err != nil {
-		return fmt.Errorf("failed to get postgres service: %w", err)
-	}
-	err = dockerClient.ServiceScale(ctx, docker.ServiceScaleOptions{
-		ServiceID:   service.ID,
-		Scale:       uint64(replicas),
-		Wait:        true,
-		WaitTimeout: timeout,
-	})
-	if err != nil {
-		return fmt.Errorf("failed to scale postgres service: %w", err)
 	}
 	return nil
 }
