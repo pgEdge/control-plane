fff7fe36a049bdaae512b5e72ca7347707b0e3a3
Jason Lynch <jason@pgedge.com>
2025-11-30 14:16:43 -0500

    chore: bump goreleaser to v2.12.7

--- tools.mk
+++ tools.mk
@@ -15,7 +15,7 @@ install-tools:
 	go install github.com/golangci/golangci-lint/v2/cmd/golangci-lint@v2.1.5
 	go install google.golang.org/protobuf/cmd/protoc-gen-go@v1.35.2
 	go install goa.design/goa/v3/cmd/goa@v3.19.1
-	go install github.com/goreleaser/goreleaser/v2@v2.9.0
+	go install github.com/goreleaser/goreleaser/v2@v2.12.7
 	go install github.com/anchore/syft/cmd/syft@v1.25.1
 	go install github.com/miniscruff/changie@v1.21.1
 	go install github.com/google/yamlfmt/cmd/yamlfmt@v0.17.0
